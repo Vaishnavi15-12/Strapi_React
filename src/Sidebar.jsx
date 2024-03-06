f382364969012336af173aea3f6f2e59a93f946e
Vaishu <vaishnavisatrasala1997@gmail.com>
2024-03-06 14:31:19 -0500

    Changes in sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import sublinks from './data';
+import { useGlobalContext } from './context';
 
 const Sidebar = () => {
+    const {isSidebarOpen,closeSidebar} = useGlobalContext();
   return (
-    <h2>
-      Sidebar
-    </h2>
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+        <div className="sidebar-container">
+            <button className='close-btn'>
+                <FaTimes/>
+            </button>
+            <div className="sidebar-links">
+                {sublinks.map((item) => {
+                    const {pageId,page,links} = item;
+                    return(
+                        <article key={pageId}>
+                            <h4>{page}</h4>
+                            <div className="sidebar-sublinks">
+                                {links.map((link) => {
+                                    const {url,icon,label,id} = link;
+                                    return(
+                                    <a key={id} href={url}>{icon}{label}</a>
+                                    );
+                                })}
+                            </div>
+                        </article>
+                    );
+
+                })}
+            </div>
+        </div>
+    </aside>
   )
 }
 
